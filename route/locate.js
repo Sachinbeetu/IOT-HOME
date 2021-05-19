d78c0b97e798ff7a12e896f18cbb772984d417ff
Gaurav Verma <tussharverma94@gmail.com>
2021-05-19 15:06:10 +0530

    changed the logic to another
    Signed-off-by: Gaurav Verma <tussharverma94@gmail.com>

--- route/locate.js
+++ route/locate.js
@@ -4,6 +4,10 @@ const ser = require('express').Router()
 
 const button_state = [
     {
+        "button_id" : '0',
+        "value" : "0"
+    },
+    {
         "button_id" : '1',
         "value" : "0"
     },
@@ -25,10 +29,6 @@ const button_state = [
     },
     {
         "button_id" : '6',
-        "value" : "0"
-    },
-    {
-        "button_id" : '7',
         "value" : "0"
     }
 ]
@@ -60,18 +60,42 @@ ser.post('/action', (req, res) =>{
 
     // console.log("button_0 : " + button_0)
     // console.log("button_1 : " + button_1)
-    // console.log("button_2 : " + button_2)
+    // console.log("typeof button_2 : " + typeof button_2)
     // console.log("button_3 : " + button_3)
     // console.log("button_4 : " + button_4)
     // console.log("button_5 : " + button_5)
+
+
     if(typeof button_0 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D1', ["1"])
+        let data_0 = ["0"]
+        for(button_data of button_state){
+            if(button_data.button_id == button_0){
+                // console.log("allowed")
+                if(button_data.value[0] === "0"){
+                    data_0.pop()
+                    data_0.push("1")
+                    button_data.value = ["1"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_0[0])
+                }else{
+                    data_0.pop()
+                    data_0.push("0")
+                    button_data.value = ["0"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_0[0])
+                }
+            }else{
+                // console.log("rejected")
+            }
+        }
+        // console.log(data_0)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D0', data_0)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                    button_ans : "Button " + button_0 + " is selected! with value = " + 1 + " and made a PUT request"
+                    button_ans : "Button " + button_0 + " is selected! with value = " + data_0[0] + " and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -81,13 +105,35 @@ ser.post('/action', (req, res) =>{
             })
     }
     if(typeof button_1 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D1', ["1"])
+        let data_1 = ["0"]
+        for(button_data of button_state){
+            if(button_data.button_id == button_1){
+                // console.log("allowed")
+                if(button_data.value[0] === "0"){
+                    data_1.pop()
+                    data_1.push("1")
+                    button_data.value = ["1"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_1[0])
+                }else{
+                    data_1.pop()
+                    data_1.push("0")
+                    button_data.value = ["0"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_1[0])
+                }
+            }else{
+                // console.log("rejected")
+            }
+        }
+        // console.log(data_1)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D1', data_1)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                   button_ans : "Button " + button_1 + " is selected! with value = " + 1 + " and made a PUT request"
+                    button_ans : "Button " + button_1 + " is selected! with value = " + data_1[0] + " and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -97,13 +143,35 @@ ser.post('/action', (req, res) =>{
             })
     }
     if(typeof button_2 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D2', ["1"])
+        let data_2 = ["0"]
+        for(button_data of button_state){
+            if(button_data.button_id == button_2){
+                // console.log("allowed")
+                if(button_data.value[0] === "0"){
+                    data_2.pop()
+                    data_2.push("1")
+                    button_data.value = ["1"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_2[0])
+                }else{
+                    data_2.pop()
+                    data_2.push("0")
+                    button_data.value = ["0"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_2[0])
+                }
+            }else{
+                // console.log("rejected")
+            }
+        }
+        // console.log(data_2)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D2', data_2)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                    button_ans : "Button " + button_2 + " is selected! with value = " + 1 + " and made a PUT request"
+                    button_ans : "Button " + button_2 + " is selected! with value = " + data_2[0] + " and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -113,13 +181,35 @@ ser.post('/action', (req, res) =>{
             })
     }
     if(typeof button_3 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D3', ["1"])
+        let data_3 = ["0"]
+        for(button_data of button_state){
+            if(button_data.button_id == button_3){
+                // console.log("allowed")
+                if(button_data.value[0] === "0"){
+                    data_3.pop()
+                    data_3.push("1")
+                    button_data.value = ["1"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_3[0])
+                }else{
+                    data_3.pop()
+                    data_3.push("0")
+                    button_data.value = ["0"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_3[0])
+                }
+            }else{
+                // console.log("rejected")
+            }
+        }
+        // console.log(data_3)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D3', data_3)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                    button_ans : "Button " + button_3 + " is selected! with value = " + 1 + " and made a PUT request"
+                    button_ans : "Button " + button_3 + " is selected! with value = " + data_3[0] + " and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -129,13 +219,35 @@ ser.post('/action', (req, res) =>{
             })
     }
     if(typeof button_4 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D4', ["1"])
+        let data_4 = ["0"]
+        for(button_data of button_state){
+            if(button_data.button_id == button_4){
+                // console.log("allowed")
+                if(button_data.value[0] === "0"){
+                    data_4.pop()
+                    data_4.push("1")
+                    button_data.value = ["1"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_4[0])
+                }else{
+                    data_4.pop()
+                    data_4.push("0")
+                    button_data.value = ["0"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_4[0])
+                }
+            }else{
+                // console.log("rejected")
+            }
+        }
+        // console.log(data_4)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D4', data_4)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                    button_ans : "Button " + button_4 + " is selected! with value = " + 1 + " and made a PUT request"
+                    button_ans : "Button " + button_4 + " is selected! with value = " + data_4[0] + " and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -145,13 +257,35 @@ ser.post('/action', (req, res) =>{
             })
     }
     if(typeof button_5 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D5', ["1"])
+        let data_5 = ["0"]
+        for(button_data of button_state){
+            if(button_data.button_id == button_5){
+                // console.log("allowed")
+                if(button_data.value[0] === "0"){
+                    data_5.pop()
+                    data_5.push("1")
+                    button_data.value = ["1"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_5[0])
+                }else{
+                    data_5.pop()
+                    data_5.push("0")
+                    button_data.value = ["0"]
+                    // console.log("button_data.value[0] = " + button_data.value[0])
+                    // console.log("WE are allowed thus changging data to = " + data_5[0])
+                }
+            }else{
+                // console.log("rejected")
+            }
+        }
+        // console.log(data_5)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D5', data_5)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                    button_ans : "Button " + button_5 + " is selected! with value = " + 1 + " and made a PUT request"
+                    button_ans : "Button " + button_5 + " is selected! with value = " + data_5[0] + " and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -161,13 +295,23 @@ ser.post('/action', (req, res) =>{
             })
     }
     if(typeof button_6 != 'undefined'){
-        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/D6', ["1"])
+        let data_6 = ["1"]
+        // for(button_data of button_state){
+        //     if(button_data.button_id == button_6){
+        //         console.log("allowed")
+                
+        //     }else{
+        //         console.log("rejected")
+        //     }
+        // }
+        // console.log(data_6)
+        axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/V1', data_6)
             .then((response_axios) => {
                 // console.log(`Status: ${response_axios.status}`)
                 // console.log("Body: ", response_axios.data)
                 // console.log("button = " + button)
                 res.render("home", {
-                   button_ans : "Smart Button is selected! with value always 1 and made a PUT request"
+                    button_ans : "Smart Switch is selected! with value always = 1 and made a PUT request"
                 })
             }).catch((err) => {
                 console.error(err)
@@ -176,48 +320,6 @@ ser.post('/action', (req, res) =>{
                 })
             })
     }
-
-            // axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/V1', data)
-            //     .then((response_axios) => {
-            //         // console.log(`Status: ${response_axios.status}`)
-            //         // console.log("Body: ", response_axios.data)
-            //         // console.log("button = " + button)
-            //         res.render("home", {
-            //             button_ans : "Button " + button_number + " is selected! with value = " + data[0] + " and made a PUT request"
-            //         })
-            //     }).catch((err) => {
-            //         console.error(err)
-            //         res.render("home", {
-            //             button_ans : "Something Went Wrong!! Try Again"
-            //         })
-            //     })
-  
-
-    // if(button_is_selected == false){
-    //     let data = ["1"]
-    //     axios.put('http://188.166.206.43/eXBZDeQdaKiOd0uVDFjpfRiTB7R6ekJL/update/V1', data)
-    //         .then((response_axios) => {
-    //             // console.log(`Status: ${response_axios.status}`)
-    //             // console.log("Body: ", response_axios.data)
-    //             // console.log("button = " + button)
-    //             res.render("home",{
-    //                 button_ans : "V1 is pressed"
-    //             })
-    //         }).catch((err) => {
-    //             console.error(err)
-    //             res.render("home", {
-    //                 button_ans : "Something Went Wrong!! Try Again"
-    //             })
-    //         })
-
-
-    // for(button of button_selection){
-    //     if(typeof button != 'undefined'){
-    //         res.render("home", {
-    //             button_ans : "Button " + button.toString() + " is selected! and made a PUT request"
-    //         })
-    //     }
-    // }
 })
 
 ser.get("/login", (req, res) =>{
